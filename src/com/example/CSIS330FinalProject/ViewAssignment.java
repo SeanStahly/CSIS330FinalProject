f659506cf94abb59bc90e783bd54e6bf1b07f24b
Sean <seanstahly@gmail.com>
2014-11-14 10:56:47 -0600

    did some stuff

---
+++ src/com/example/CSIS330FinalProject/ViewAssignment.java
@@ -0,0 +1,7 @@
+package com.example.CSIS330FinalProject;
+
+/**
+ * Created by sean on 11/12/14.
+ */
+public class ViewAssignment {
+}
--- src/com/example/CSIS330FinalProject/ViewSetup.java
+++ src/com/example/CSIS330FinalProject/ViewSetup.java
@@ -1,18 +1,40 @@
 package com.example.CSIS330FinalProject;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
 
 /**
  * Created by sean on 11/10/14.
  */
-public class ViewSetup extends Activity {
-
+public class ViewSetup extends ListActivity {
+    public static final String ROW_ID= "row_id";
+    private ListView snakeViewList;
+    private CursorAdapter snakeAdapter;
 
     @Override
     public void onCreate(Bundle savedInsanceState) {
         super.onCreate(savedInsanceState);
+        snakeViewList = getListView();
+        snakeViewList.setOnItemClickListener(viewAssignmentListner);
+
+        String[] from =  new String[] { "name" };
+//        int[] to = new int[] {R.id.assignmentSetupView};
 
 
     }
+
+    AdapterView.OnItemClickListener viewAssignmentListner = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Intent viewAssignment = new Intent(ViewSetup.this, ViewAssignment.class);
+
+            viewAssignment.putExtra(ROW_ID, l);
+            startActivity(viewAssignment);
+        }
+    };
 }
